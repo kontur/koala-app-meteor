d03ab9bcde51740dae329a86ea75ef4aebda338c
Johannes Neumeier <hello@johannesneumeier.com>
2015-01-18 19:34:48 +0200

    initial tests for instgra login and api calls to local python api

---
+++ koala-app.js
@@ -0,0 +1,34 @@
+if (Meteor.isClient) {
+
+    Meteor.subscribe("users");
+
+    console.log("startup", Meteor.user());
+
+    Tracker.autorun(function () {
+        var foo = Meteor.user();
+        console.log("autorun", Meteor.user(), foo ? foo.services.instagram.accessToken : null);
+
+    });
+
+    console.log(Meteor.call("test", function (err, res) {
+        console.log("err", err, "res", res);
+        console.log(JSON.parse(res.content));
+    }));
+
+}
+
+if (Meteor.isServer) {
+    Meteor.publish("users", function () {
+        return Meteor.users.find(this.userId, {fields: {"services.instagram.accessToken": 1}});
+    });
+
+    Meteor.methods({
+        "test": function () {
+            //return "hello test " + (Meteor.user().services ? Meteor.user().services.instagram.accessToken : null);
+            return Meteor.http.call("GET", "http://0.0.0.0:5000/api/venues/show/60.1896861/24.8386975?access_token=" +
+            (Meteor.user().services ? Meteor.user().services.instagram.accessToken : null));
+        }
+    });
+}
+
+
---
+++ server/accounts.js
@@ -0,0 +1,7 @@
+ServiceConfiguration.configurations.remove({service: "instagram"});
+ServiceConfiguration.configurations.insert({
+    service: "instagram",
+    clientId: "3df64daf6db4424e8422455e3697085f",
+    scope: "basic",
+    secret: "d3162283133a403d89a8b41397bec160"
+});
