15f7c6262f29baf24269a7849d1ec1579ace8a7f
Johannes Neumeier <hello@johannesneumeier.com>
2015-02-13 15:22:54 +0200

    start work on wireframes v2, frontpage infinite scroll

--- meteor/client/modules/Home/Home.js
+++ meteor/client/modules/Home/Home.js
@@ -1,65 +1,93 @@
 if (Meteor.isClient) {
 
     Template.Home.created = function () {
-        this.popular = new ReactiveVar();
-        this.trending = new ReactiveVar();
-        this.trendsetters = new ReactiveVar();
-        this.network = new ReactiveVar();
+        this.discover = new ReactiveVar([]);
+        this.numVenues = new ReactiveVar(0);
+        this.maxVenues = new ReactiveVar(3);
     };
 
     Template.Home.helpers({
-        "popular": function () {
-            return Template.instance().popular.get();
-        },
-        "trending": function () {
-            return Template.instance().trending.get();
-        },
-        "trendsetters": function () {
-            return Template.instance().trendsetters.get();
-        },
-        "network": function () {
-            return Template.instance().network.get();
+        "discover": function () {
+            return Template.instance().discover.get();
+        }
+    });
+
+
+    Template.Home.events({
+        "click .load-more": function (event, tpl) {
+            // increase the number of max venues, this will trigger autorun to check
+            // if enough venues have been loaded
+            tpl.maxVenues.set(tpl.maxVenues.get() + 3);
         }
     });
 
     Template.Home.rendered = function () {
         var tpl = Template.instance();
 
-        navigator.geolocation.getCurrentPosition(function (position) {
-            Session.set("geolocation", position);
-
-            Meteor.call("venues", position.coords.latitude, position.coords.longitude, { limit: 2 }, function (err, res) {
-                if (err) {
-                    Session.set("errors", _.union(Session.get("errors"), [err]));
-                    return;
-                }
-                tpl.popular.set(JSON.parse(res.content));
-            });
-
-            Meteor.call("trendsetters", position.coords.latitude, position.coords.longitude, { limit: 4 }, function (err, res) {
-                if (err) {
-                    Session.set("errors", _.union(Session.get("errors"), [err]));
-                    return;
-                }
-                tpl.trendsetters.set(JSON.parse(res.content));
-            });
-        });
-
-        // hard coded new york for demo :O
-        Meteor.call("venues", 40.7033121, -73.979681, { limit: 2 }, function (err, res) {
-            if (err) {
-                Session.set("errors", _.union(Session.get("errors"), [err]));
-                return;
+        // where the magic happens!
+        // if either numVenues of maxVenues change, this triggers loading of new venues if so needed
+        // note: this also autoruns on first init, because the variables "change"
+        Tracker.autorun(function () {
+            if (tpl.numVenues.get() < tpl.maxVenues.get()) {
+                getVenue(tpl.numVenues.get());
             }
-            tpl.trending.set(JSON.parse(res.content));
         });
 
-        Meteor.call("network", function (err, res) {
-            if (err) {
-                Session.set("errors", _.union(Session.get("errors"), [err]));
-                return;
+        $(window).scroll(onScroll);
+
+        function getVenue(offset) {
+            console.log("getVenue", offset);
+
+            navigator.geolocation.getCurrentPosition(function (position) {
+                Session.set("geolocation", position);
+
+                Meteor.call("venues", position.coords.latitude, position.coords.longitude,
+                    {
+                        limit: 1,
+                        offset: offset,
+                        images: 1
+                    },
+                    function (err, res) {
+                    if (err) {
+                        Session.set("errors", _.union(Session.get("errors"), [err]));
+                        return;
+                    }
+
+                    console.log("res", JSON.parse(res.content)[0]);
+
+                    // push new venues to view helper and increase count
+                    var current = tpl.discover.get();
+                    current.push(JSON.parse(res.content)[0]);
+                    tpl.discover.set(current);
+                    tpl.numVenues.set(tpl.numVenues.get() + 1);
+                });
+            });
+        }
+
+        function onScroll(event) {
+            console.log("scrolling");
+            var windowH = $(window).height(),
+                documentH = $(document).height(),
+                marker = $(".loading-more").offset().top,
+                position = $(document).scrollTop();
+
+            console.log(windowH, documentH, marker, position);
+
+
+            // if the marker is less than 1 windowH below the fold: load more
+
+            if (position + windowH > documentH - windowH) {
+                console.log("load more");
+                if (tpl.numVenues.get() < tpl.maxVenues.get()) {
+                    console.log("already loading more");
+                } else {
+                    console.log("LOAD MORE");
+                    tpl.maxVenues.set(tpl.maxVenues.get() + 3);
+                }
+            } else {
+                console.log("leave it be");
             }
-            tpl.network.set(JSON.parse(res.content));
-        });
+
+        }
     };
 }
